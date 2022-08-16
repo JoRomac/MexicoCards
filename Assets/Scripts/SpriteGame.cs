57c9e53bce8692bc140fd9f1b3bc4a12dff95a91
Josip Romac <josipromac@gmail.com>
2022-08-16 14:46:07 +0200

    Sprite Game add

--- Assets/Scripts/SpriteGame.cs
+++ Assets/Scripts/SpriteGame.cs
@@ -19,12 +19,10 @@ public class SpriteGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
